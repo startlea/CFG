47aab7acef03e823648226d02992c67860605a2e
startlea <eos1984kowalska@gmail.com>
2023-03-25 10:10:07 +0100

    Next def

--- Microsoft.py
+++ Microsoft.py
@@ -1,14 +1,24 @@
 print("Hello Beautiful!")
-
 def how_r_u():
     how_r_u = input("How are you doing today? [good/ok/bad] ")
     if how_r_u == "good":
-        print("That's great! I'm happy for you.")
+        print("That's great! \nI'm happy for you.")
     elif how_r_u == "ok":
-        print("That's ok, it will be good. I'm crossing my fingers on it")
+        print("That's ok, it will be good. \nFingers crossed for it")
     elif how_r_u == "bad":
-        print("Ooh I'm so sorry for you. Hugging you tightly so you could feel better")
+        print("Ooh I'm so sorry for you. \nHugging you tightly so you could feel better")
     else:
-        print("Check your answer again")
-    
+        print("Check your answer")
+
+def repeat():
+    repeat = input("Do you want to repeat?[yes/no] ")
+    if repeat == "yes":
+        how_r_u()
+        print("Thank you! Have a nice day.") 
+    elif repeat == "no":
+        print("Thank you! Have a nice day.")  
+    else:
+        print("Check your answer")
+         
 how_r_u()
+repeat()
