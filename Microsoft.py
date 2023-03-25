4b35f96c27036e1cf434a13f74ca9c5c8ddf2b10
startlea <eos1984kowalska@gmail.com>
2023-03-25 08:39:35 +0100

    New file Microsoft Linkedin Course

---
+++ Microsoft.py
@@ -0,0 +1,13 @@
+print("Hello Beautiful!")
+how_r_u = input("How are you doing today? [good/ok/bad] ")
+
+if how_r_u == "good":
+    print("That's great! I'm happy for you.")
+elif how_r_u == "ok":
+    print("That's ok, it will be good. I'm crossing my fingers on it")
+elif how_r_u == "bad":
+    print("Ooh I'm so sorry for you. Hugging you tightly so you could feel better")
+else:
+    print("Check your answer again")
+
+
